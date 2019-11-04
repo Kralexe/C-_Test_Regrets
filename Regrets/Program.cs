8d5678de94ab00f0ba110f01daa9710750b626c4
La <rer@ya.ru>
2019-11-04 18:47:23 +0300

    first commit

---
+++ Regrets/Program.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Diagnostics;
+
+
+namespace Regrets
+{
+    public class Class1
+    {
+        public static ArrayList ListUsers(HttpClient client, string url)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            var response = client.GetAsync(url).Result.Content.ReadAsStringAsync().Result;
+            timer.Stop();
+            TimeSpan timeTaken = timer.Elapsed;
+            string[] split = Convert.ToString(timeTaken.TotalSeconds).Split(".");
+            int sec = Convert.ToInt32(split[0]);
+            JToken jArr = JsonConvert.DeserializeObject<JToken>(response);
+            ArrayList result = new ArrayList
+            {
+                jArr["page"],
+                jArr["per_page"],
+                jArr["total"],
+                jArr["total_pages"],
+                jArr["data"][4]["first_name"],
+                sec
+            };
+            Console.WriteLine(sec);
+            return result;
+        }
+
+        public static dynamic GetUser(HttpClient client, string url)
+        {
+            var response = client.GetAsync(url).Result.Content.ReadAsStringAsync().Result;
+            JToken jArr = JsonConvert.DeserializeObject<JToken>(response);
+            if (Convert.ToString(client.GetAsync(url).Result.StatusCode) == "OK")
+            {
+                var person = Tuple.Create(jArr["data"]["id"],
+                jArr["data"]["email"],
+                jArr["data"]["first_name"],
+                jArr["data"]["last_name"],
+                jArr["data"]["avatar"]
+            );
+            return person; }
+            else { return "404"; }
+            
+        }
+
+        
+    }
+}
---
+++ RegretsBusinessLogic.Tests/Program.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using Regrets;
+using System.Collections;
+using NUnit.Framework;
+
+namespace Regrets.Tests
+{
+    class Program
+    {
+        [TestFixture]
+        public class RegretsTests
+        {
+            
+            [TestCase]
+            public void When_ListUsers_Expects_IsAllFields_Present()
+            {
+                var handler = new HttpClientHandler();
+                var client = new HttpClient(handler);
+                ArrayList vector = Class1.ListUsers(client, "https://reqres.in/api/users?page=2");
+                
+                Assert.That(Convert.ToInt32(vector[0]) == 2 && Convert.ToInt32(vector[1]) == 6 && Convert.ToInt32(vector[2]) == 12 && Convert.ToInt32(vector[3]) == 2 && Convert.ToString(vector[4])=="George");
+            }
+
+            [TestCase]
+            public void When_GetUser_Expects_IsAllFields_Present()
+            {
+                var handler = new HttpClientHandler();
+                var client = new HttpClient(handler);
+                var vector = Class1.GetUser(client, "https://reqres.in/api/users/2");
+
+                Assert.That(Convert.ToInt32(vector.Item1) == 2 && Convert.ToString(vector.Item2) == "janet.weaver@reqres.in" && Convert.ToString(vector.Item3) == "Janet" && Convert.ToString(vector.Item4) == "Weaver" && Convert.ToString(vector.Item5) == "https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg");
+            }
+
+            [TestCase]
+            public void When_GetUser_Expects_Is404()
+            {
+                var handler = new HttpClientHandler();
+                var client = new HttpClient(handler);
+                var vector = Class1.GetUser(client, "https://reqres.in/api/users/23");
+
+                Assert.That(Convert.ToString(vector)=="404");
+            }
+            [TestCase]
+            public void When_GetUser_Expects_DelayIs3Sec()
+            {
+                var handler = new HttpClientHandler();
+                var client = new HttpClient(handler);
+                var vector = Class1.ListUsers(client, "https://reqres.in/api/users?delay=3");
+
+                Assert.That(Convert.ToInt32(vector[5])>2);
+            }
+        }
+
+        static void Main()
+        {
+            var handler = new HttpClientHandler();
+            var client = new HttpClient(handler);
+            var vector = Class1.GetUser(client, "https://reqres.in/api/users/2");
+            Console.WriteLine(vector);
+            
+
+            
+
+
+
+        }
+    }
+}
